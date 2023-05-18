be868d9ef2fdfa00e2750e8b50aba50d29ca86b7
Bryan Colque Clavijo <Bryan.Colque@jala.university>
2023-05-18 18:21:43 -0400

    Task 3, changes added to unitTest file and package.json file

--- AT_20_GRAPHQL_USER_SERVICE/tests/unitTest.test.js
+++ AT_20_GRAPHQL_USER_SERVICE/tests/unitTest.test.js
@@ -2,6 +2,6 @@ describe ( 'This is the test suite for the graphQl-user service' , () => {
     it ('Should display a message indicating success', ()=> {
         const message = 'Connected successfully';
         const expMessage = 'Connected successfully';
-        expect(message),toBe(expMessage); 
+        expect(message).toBe(expMessage); 
     });
 });
